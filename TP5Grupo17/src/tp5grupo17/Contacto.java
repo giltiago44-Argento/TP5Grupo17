e8d4c7b9bccbdad8aead18bf7f5f50903935726f
giltiago44-Argento <giltiago44@gmail.com>
2026-09-16 11:25:59 -0300

    Clase Directorio y contacto agregadas
    clase directorio y Contacto con todos los metodos propuestos en consigna hechos

---
+++ TP5Grupo17/src/tp5grupo17/Contacto.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp5grupo17;
+
+/**
+ *
+ * @author Agos
+ */
+public class Contacto {
+    private int dni;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String ciudad;
+
+ 
+
+    
+    
+
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+}
---
+++ TP5Grupo17/src/tp5grupo17/Directorio.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp5grupo17;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author Agos
+ */
+public class Directorio {
+   private  Map<Long,Contacto> lista_clientes = new TreeMap<>(); 
+
+    public Directorio() {
+    
+    }
+
+    public void agregarContacto(Long telefono,Contacto contacto){
+    lista_clientes.put(telefono, contacto);
+    }
+    
+    public Contacto buscarContacto(Long telefono){
+    
+        return lista_clientes.get(telefono);
+    }
+    
+    
+    
+    public Set<Long> buscarTelefono(String apellido){
+    Set<Long> telefonos = new HashSet<>();
+    
+        for (Long telefono : lista_clientes.keySet()) {
+            Contacto c = lista_clientes.get(telefono);
+            if (c.getApellido().equalsIgnoreCase(apellido)) {
+                telefonos.add(telefono);
+            }
+        }
+        return telefonos;
+    }
+    
+    public ArrayList<Contacto> buscarContactos(String Ciudad){
+    ArrayList<Contacto> c = new ArrayList<>();
+    
+    for (Long telefono : lista_clientes.keySet()) {
+         Contacto contacto = lista_clientes.get(telefono);
+         if (contacto.ge) {
+            
+        }
+        }
+    
+    return c;
+    }
+    
+    public Map<Long, Contacto> getLista_clientes() {
+        return lista_clientes;
+    }
+
+    
+    
+    
+}
